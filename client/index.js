68c495ee69aca52daada27f138586ab811ead71b
Vartan <vartana@gmail.com>
2016-03-23 13:48:13 -0700

    sigint

--- client/index.js
+++ client/index.js
@@ -2,7 +2,7 @@ var shell = require('shelljs');
 var uuid = shell.exec('sudo blkid -s UUID -o value /dev/mmcblk0p2', {silent:true}).stdout;
 
 // Prepare GPIO ports.
-var libRelay = new require('./relay');
+var libRelay = require('./relay');
 console.log(libRelay);
 libRelay.Init();
 
