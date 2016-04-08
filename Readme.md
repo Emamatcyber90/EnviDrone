20ac1756f8540acc6d120839b754fb38c9cc2f16
Vartan <vartana@gmail.com>
2016-04-08 16:09:04 -0700

    cozir

---
+++ Readme.md
@@ -0,0 +1,16 @@
+
+## CO2
+  Light Time
+    -1200 - CO2 RELEASE
+  Night Time
+    -500 - FAN
+
+## Humidity
+ High
+    - co2 off
+    - fan on
+
+## Temp
+  High
+    - co2 off
+    - fan on
--- client/index.js
+++ client/index.js
@@ -1,62 +1,75 @@
-var shell = require('shelljs');
-var uuid = shell.exec('sudo blkid -s UUID -o value /dev/mmcblk0p2', {silent:true}).stdout;
-
-// Prepare GPIO ports.
-var relayInit = require('./relay').Init;
-relayInit();
-
-// ZeroConfig
-var PORT = 6024;
-var MULTICAST_ADDR = '239.255.255.250';
-var dgram = require('dgram');
-var client = dgram.createSocket('udp4');
-
-var io = {};
-
-client.on('listening', function () {
-    var address = client.address();
-    console.log('UDP Client listening on ' + address.address + ":" + address.port);
+var cozirDriver = require('./cozirDriver');
+var sensor = cozirDriver({
+  "port": "/dev/ttyAMA0",
+  "feedId": "Drone"
 });
 
-client.on('message', function (message, rinfo) {
-    console.log('Message from: ' + rinfo.address + ':' + rinfo.port + ' - ' + message);
-    bindSocketIO('http://' + rinfo.address + ':7777');
+sensor.start();
+
+sensor.on('data', function(feedId, data){
+  console.log(feedId, data);
+
 });
 
-client.bind(PORT, function () {
-    console.log('add addMembership');
-    client.addMembership(MULTICAST_ADDR);
-});
+// var shell = require('shelljs');
+// var uuid = shell.exec('sudo blkid -s UUID -o value /dev/mmcblk0p2', {silent:true}).stdout;
+
+// // Prepare GPIO ports.
+// var relayInit = require('./relay').Init;
+// relayInit();
+
+// // ZeroConfig
+// var PORT = 6024;
+// var MULTICAST_ADDR = '239.255.255.250';
+// var dgram = require('dgram');
+// var client = dgram.createSocket('udp4');
+
+// var io = {};
+
+// client.on('listening', function () {
+//     var address = client.address();
+//     console.log('UDP Client listening on ' + address.address + ":" + address.port);
+// });
+
+// client.on('message', function (message, rinfo) {
+//     console.log('Message from: ' + rinfo.address + ':' + rinfo.port + ' - ' + message);
+//     bindSocketIO('http://' + rinfo.address + ':7777');
+// });
+
+// client.bind(PORT, function () {
+//     console.log('add addMembership');
+//     client.addMembership(MULTICAST_ADDR);
+// });
 
 
-// Socket.IO
-function bindSocketIO(host){
-  io = require('socket.io-client')(host);
+// // Socket.IO
+// function bindSocketIO(host){
+//   io = require('socket.io-client')(host);
 
-  io.on('connect', function(){
-    console.log("Connected");
-    client.dropMembership(MULTICAST_ADDR);
+//   io.on('connect', function(){
+//     console.log("Connected");
+//     client.dropMembership(MULTICAST_ADDR);
 
-    io.emit('private message', 'Hello World');
-    GetPPM();
-  });
+//     io.emit('private message', 'Hello World');
+//     GetPPM();
+//   });
 
-  io.on('event', function(data){
-    console.log("Event", data);
-  });
+//   io.on('event', function(data){
+//     console.log("Event", data);
+//   });
 
-  io.on('disconnect', function(){
-    console.log("Disconnect");
-    client.addMembership(MULTICAST_ADDR);
-  });
-}
+//   io.on('disconnect', function(){
+//     console.log("Disconnect");
+//     client.addMembership(MULTICAST_ADDR);
+//   });
+// }
 
-var co2 = require("./co2");
+// var co2 = require("./co2");
 
-function GetPPM(){
-  co2.on('data',function(conc, temp_co2){
-    console.log("CO2 Conc: ", conc, " Temp: ", temp_co2);
-    var readings = "CO2 Conc: " + conc + " Temp: " + temp_co2;
-    io.emit('private message', readings);
-  });
-}
+// function GetPPM(){
+//   co2.on('data',function(conc, temp_co2){
+//     console.log("CO2 Conc: ", conc, " Temp: ", temp_co2);
+//     var readings = "CO2 Conc: " + conc + " Temp: " + temp_co2;
+//     io.emit('private message', readings);
+//   });
+// }
