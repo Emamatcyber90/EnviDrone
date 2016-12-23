64adc6668086dddf7d85a7b66e06483183f475b0
Vartan Arabyan <vartana@gmail.com>
2016-12-23 13:35:18 -0700

    Update index.js

--- config/index.js
+++ config/index.js
@@ -23,8 +23,8 @@ if (Object.keys(settings).length === 0) {
     //Create default settings
     settings.carbon = 0;
     settings.humidity = 40;
-    settings.offTime = moment(offTime)->format("HH:mm");
-    settings.lightOn = moment(lightOn)->format("HH:mm");
+    settings.offTime = moment(offTime).format("HH:mm");
+    settings.lightOn = moment(lightOn).format("HH:mm");
     settings.lightOff = 0;
     settings.fanOnStep = 800;
     settings.tmpStep = 110;
