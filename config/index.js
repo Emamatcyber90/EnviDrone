2a498b5f0e49981e5cc338b4fa2b83f2620aaf1c
Suren Gasparyan <suren.gasparyan1997@gmail.com>
2016-12-24 11:12:12 +0400

    Fix start bug

--- config/index.js
+++ config/index.js
@@ -15,17 +15,12 @@ var bjson = require('bjson'),
 
 
 if (Object.keys(settings).length === 0) {
-    var lightOn = new Date().setHours(20)
-    lightOn = new Date(lightOn).setMinutes(0)
-    lightOn = new Date(lightOn).setSeconds(0)
-    var offTime = new Date(lightOn).setHours(new Date(lightOn).getHours() + 11)
-
     //Create default settings
     settings.carbon = 0;
     settings.humidity = 40;
-    settings.offTime = moment(offTime).format("HH:mm");
-    settings.lightOn = moment(lightOn).format("HH:mm");
-    settings.lightOff = 0;
+    settings.offTime = "07:00";
+    settings.lightOn = "20:00";
+    settings.lightOff = 11;
     settings.fanOnStep = 800;
     settings.tmpStep = 110;
     settings.waterCycle = 120;
