5bab3a381a28251d7e53cc81377c8871c842928a
Suren Gasparyan <suren.gasparyan1997@gmail.com>
2017-01-24 15:27:35 +0400

    Remove for test

--- config/index.js
+++ config/index.js
@@ -9,34 +9,5 @@ var settings = bjson('settings', function(observe) {
     observe.on('change', function(changes) {})
 });
 
-var newSettings = {
-    "carbon": 0,
-    "humidity": 40,
-    "lightOn": "20:00",
-    "lightOff": 11,
-    "fanOnStep": 800,
-    "tmpStep": 110,
-    "waterCycle": 120,
-    "waterDuration": 3.5,
-    "statuses": {
-        "light": false,
-        "carbon": false,
-        "water": false,
-        "fan": false
-    },
-    "version": 1
-}
-for (var i in newSettings) {
-    if (!settings[i]) {
-        settings[i] = newSettings[i];
-    }
-}
-
-if (settings.lightOn.toString().length == 2) {
-    settings.lightOn = settings.lightOn + ":00"
-} else if (settings.lightOn.toString().length == 1) {
-    settings.lightOn = "0" + settings.lightOn + ":00"
-}
-
 module.exports.config = settings;
 module.exports.observe = observer
