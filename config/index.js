75bfd9bc43d7553c64693ca5fa83f3e87e6ad12e
Vartan Arabyan <vartana@gmail.com>
2017-02-03 13:51:11 -0700

    Update index.js

--- config/index.js
+++ config/index.js
@@ -29,11 +29,6 @@ var newSettings = {
 if (Object.keys(settings).length === 0) {
     settings = newSettings
 }
-for (var i in newSettings) {
-    if (!settings[i]) {
-         settings[i] = newSettings[i];
-     }
- }
 
 module.exports.config = settings;
 module.exports.observe = observer
