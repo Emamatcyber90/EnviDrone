c7fa7e42d47cda9035d2363268d28f30b1fae785
Vartan Arabyan <vartana@gmail.com>
2017-02-03 15:59:29 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,59 +1,16 @@
-// // Prepare GPIO ports.
-// var relayInit = require('./relay').Init;
-// relayInit();
+    var serialModule = require("serialport");
+    var port = "/dev/ttyS0";
+    var delimiter = "\r\n";
 
-// // ZeroConfig
-// var PORT = 6024;
-// var MULTICAST_ADDR = '239.255.255.250';
-// var dgram = require('dgram');
-// var client = dgram.createSocket('udp4');
-
-// var io = {};
-
-// client.on('listening', function () {
-//     var address = client.address();
-//     console.log('UDP Client listening on ' + address.address + ":" + address.port);
-// });
-
-// client.on('message', function (message, rinfo) {
-//     console.log('Message from: ' + rinfo.address + ':' + rinfo.port + ' - ' + message);
-//     bindSocketIO('http://' + rinfo.address + ':7777');
-// });
-
-// client.bind(PORT, function () {
-//     console.log('add addMembership');
-//     client.addMembership(MULTICAST_ADDR);
-// });
+    serialPort = new serialModule.SerialPort(port, {
+        parser: serialModule.parsers.readline(delimiter),
+        baudrate: 9600
+    }, false);
 
 
-// // Socket.IO
-// function bindSocketIO(host){
-//   io = require('socket.io-client')(host);
 
-//   io.on('connect', function(){
-//     console.log("Connected");
-//     client.dropMembership(MULTICAST_ADDR);
-
-//     io.emit('private message', 'Hello World');
-//     GetPPM();
-//   });
-
-//   io.on('event', function(data){
-//     console.log("Event", data);
-//   });
-
-//   io.on('disconnect', function(){
-//     console.log("Disconnect");
-//     client.addMembership(MULTICAST_ADDR);
-//   });
-// }
-
-// var co2 = require("./co2");
-
-// function GetPPM(){
-//   co2.on('data',function(conc, temp_co2){
-//     console.log("CO2 Conc: ", conc, " Temp: ", temp_co2);
-//     var readings = "CO2 Conc: " + conc + " Temp: " + temp_co2;
-//     io.emit('private message', readings);
-//   });
-// }
+    serialPort.open(function(err) {
+        serialPort.on("data", function(data) {
+          console.log("Data:", data);
+        });
+    });
