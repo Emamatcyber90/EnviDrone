93fcfe1037a1d1b637fb005c9784eeec575b7f65
Suren Gasparyan <suren.gasparyan1997@gmail.com>
2017-02-17 19:54:08 +0400

    Change to moment

--- client/TimeService.js
+++ client/TimeService.js
@@ -5,7 +5,6 @@ function CheckDates(time, houre) {
 
     var currentDate = new Date();
 
-
     var timeFromDay = (new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate())).setHours(0).setMinutes(0).setSeconds(0).getTime();
 
     var allTime = time.split(":");
@@ -19,4 +18,21 @@ function CheckDates(time, houre) {
     return (endTime - dayTime > 0 && endTime - dayTime > currentTime) || (startTime < currentTime && endTime > currentTime)
 }
 
+
+function CheckDates(time, houre) {
+    var dayTime = (60*24*1000) * 60;
+
+    var toDayTime = moment(new Date()).hours(0).minutes(0).valueOf();
+
+    var allTime = time.split(":");
+
+    var startTime = moment().hours(allTime[0]).minutes(allTime[1]).valueOf() - toDayTime;
+
+    var endTime = startTime + (houre * 60 * 60 * 1000);
+
+    var currentTime = moment(new Date()).valueOf() - toDayTime;
+
+    return (endTime - dayTime > 0 && endTime - dayTime > currentTime) || (startTime < currentTime && endTime > currentTime)
+
+}
 module.exports.CheckDates = CheckDates;
