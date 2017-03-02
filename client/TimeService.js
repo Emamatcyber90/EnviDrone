323f381b557c07166a788594ad0ca13cd11906d6
Suren Gasparyan <suren.gasparyan1997@gmail.com>
2017-03-02 20:18:02 +0400

    Change time < condition

--- client/TimeService.js
+++ client/TimeService.js
@@ -13,6 +13,6 @@ function CheckDates(time, houre) {
 
     var currentTime = moment(new Date()).valueOf() - toDayTime;
 
-    return (endTime - dayTime > 0 && endTime - dayTime > currentTime) || (startTime < currentTime && endTime > currentTime)
+    return (endTime - dayTime > 0 && endTime - dayTime > currentTime) || (startTime <= currentTime && endTime > currentTime)
 }
 module.exports.CheckDates = CheckDates;
