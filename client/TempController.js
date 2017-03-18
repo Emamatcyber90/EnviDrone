dc48208eaf6f8356eb4ef346f92ad168dd0344e0
Suren Gasparyan <suren.gasparyan1997@gmail.com>
2017-03-18 10:53:48 +0400

    Change condition

--- client/TempController.js
+++ client/TempController.js
@@ -12,20 +12,21 @@ var temp = function() {
 
     var analyze = function(data) {
         var tmp = (data * 9 / 5) + 32;
-        if (settings.config.manual.status && data >= settings.config.manual.tmpOnStep) {
-            FanController.on();
-        }
-        if (tmp >= settings.config.tmpStep) {
-            var newDate = Number(new Date()) - 5000;
-            if (time <= newDate) {
+        var newDate = Number(new Date()) - 5000;
+        console.log(socket)
+        if (time <= newDate) {
+            if (settings.config.manual.status && data >= settings.config.manual.tmpOnStep) {
+                FanController.on();
+            }
+            if (tmp >= settings.config.tmpStep) {
                 settings.config.tmpStepStatus = true
                 settings.config.switchStatus = true;
                 OFF();
-                time = Number(new Date())
+            } else {
+                settings.config.tmpStepStatus = false
+                settings.config.switchStatus = false;
             }
-        } else {
-            settings.config.tmpStepStatus = false
-            settings.config.switchStatus = false;
+            time = Number(new Date())
         }
     }
     return analyze;
